83ac40c0d36f2051d030d0c3128d8ae0763d810a
John Yu <goldtruffle2015@hotmail.com>
2020-05-19 19:53:44 -0600

    Completed the javascript code

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Rock_Paper_Scissors_Web
+# What is the HTML DOM?
+It is a way to modify html elements.
+
+# Methods 
+HTML DOM methods are <b>actions</b> you can perform
+
+HTML DOM properties are <b>values</b> that you can set or change
+
+Methods include:
+- getElementById()
+    - accesses HTML elements
+- innerHTML Property
+    - access the content of the element
+
+# JavsScript HTML DOM Document
+The document object represents your web page. To access elements you start by accessing the document object ("document").
+
+Finding HTML Elements:
+- document.getElementById(id)
+    - Find an element by element id
+- document.getElementsByTagName(name)
+    - Find elements by tag name
+- document.getElementsByClassName(name)
+    - Find elements by class name
+
+Changing HTML elements:
+- element.innerHTML = new html content
+    - Change the inner HTML of an element
+- element.attribute = new value
+    - Change the attribute value of an HTML element
+- element.style.property = new style
+    - Chagne the style of an HTML element
+- element.setAttribute(attribute, value)
+    - Change the attribute value of an HTML element
+
+Adding and Deleting Elements:
+- document.createElement(element)
+    - Create an HTML element
+- document.removeChild(element)
+    - Remove an HTML element
+- document.appendChild(element)
+    - Add an HTML element
+- document.replaceChild(new, old)
+    - Replace an HTML element
+- document.write(text)
+    - Write into the HTML output stream
+
+Adding Events Handlers:
+- Document.getElementById(id).onclick = function(){code}
+    - Adding event handler code to an onclick event
--- app.js
+++ app.js
@@ -1,1 +1,94 @@
-console.log("hello");
+// -- Cacheing the variables -- //
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");   // DOM Variables
+const computerScore_span = document.getElementById("computer-score");   // DOM Variables
+const scoreBoard_div = document.querySelector(".score-board");  // Returns first element which matches the input
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
+
+// -- Functions -- //
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3)
+    return choices[randomNumber];
+}
+
+function convertToWord(letter) {
+    var dict = {
+        "r": "Rock",
+        "p": "Paper",
+        "s": "Scissors"
+    }
+    return dict[letter];
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML =`${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!"`;
+    userChoice_div.classList.add('green-glow');
+    setTimeout(function() {userChoice_div.classList.remove('green-glow')}, 300)
+}
+
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    userScore_span.innerHTML = userScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML =`${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}. You lose!"`;
+    userChoice_div.classList.add('red-glow');
+    setTimeout(function() {userChoice_div.classList.remove('red-glow')}, 300)
+}
+
+function draw(userChoice, computerChoice) {
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML =`${convertToWord(userChoice)}${smallUserWord} draws to ${convertToWord(computerChoice)}${smallCompWord}."`;
+    userChoice_div.classList.add('gray-glow');
+    setTimeout(function() {userChoice_div.classList.remove('gray-glow')}, 300)
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
+
+// -- Main -- //
+function main() {
+    // -- Event Listeners -- //
+    rock_div.addEventListener("click", () => game("r"))
+
+    paper_div.addEventListener("click", () => game("p"))
+
+    scissor_div.addEventListener("click", () => game("s"))
+}
+
+main();
